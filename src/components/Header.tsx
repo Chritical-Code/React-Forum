deeddff312acb0f2a8c4b27dda1063d6c57c7fb6
chritical <christopherkbanks@gmail.com>
2026-02-02 13:21:23 -0500

    The tailwind errors seem to have fixed themselves.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,19 +7,19 @@ export default function Header(){
 
     return(
         <div className="flex items-center w-full h-14 shrink-0 bg-yellow-500">
-            <div className="flex w-1/4 items-center bg-blue-500">
+            <div className="flex w-1/4 items-cente">
                 <HeaderButton link="/" text="⌂" />
                 <h1 className="text-3xl font-bold text-center">
                     Forum
                 </h1>
             </div>
 
-            <div className="flex grow justify-center items-center bg-green-500">
+            <div className="flex grow justify-center items-center">
                 <input type="text" className="m-1 min-w-0 w-1/3 bg-white border rounded-lg pl-2 pr-2"></input>
                 <p className="m-1">🔍</p>
             </div>
 
-            <div className="flex w-1/4 items-center justify-end bg-red-500">
+            <div className="flex w-1/4 items-center justify-end">
                 <HeaderButton link="/" text="⚙" />
                 <HeaderButton link="/user/bob" text="☺" />
             </div>
