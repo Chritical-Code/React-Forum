dc58cd4ad14d14358addf1b946acc4798f916457
chritical <christopherkbanks@gmail.com>
2026-03-17 14:16:08 -0400

    Stopped postbox text from overflowing

--- src/components/PostBox.tsx
+++ src/components/PostBox.tsx
@@ -15,8 +15,8 @@ export default async function PostBox({post, viewOrEdit}: PostBoxProps){
     return(
         <a href={"/post/" + viewOrEdit + "/" + post.id}>
             <div className="flex flex-col items-center shrink-0 bg-gray-400 w-121 h-80 border m-2 overflow-hidden">
-                <p className="font-bold">{post.title}</p>
-                <p>{post.text}</p>
+                <p className="font-bold h-6 w-11/12 overflow-hidden text-center">{post.title}</p>
+                <p className="h-6 w-11/12 overflow-hidden text-center">{post.text}</p>
                 {image && <MyImg image={image}></MyImg>}
             </div>
         </a>
