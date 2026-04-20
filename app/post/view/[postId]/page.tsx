d7f8b6f7e5f734ff2de29ca12ea0089d0b337a83
chritical <christopherkbanks@gmail.com>
2026-04-20 14:45:06 -0400

    Added profile pics to comments.

--- app/post/view/[postId]/page.tsx
+++ app/post/view/[postId]/page.tsx
@@ -40,7 +40,7 @@ export default async function ViewPost({params}: ViewPostProps){
 
     const comments = commentData.map((comment, index) => {
         return(
-            <CommentComponent key={index} username={comment.author.name} text={comment.text} userId={comment.authorId}></CommentComponent>
+            <CommentComponent key={index} username={comment.author.name} text={comment.text} userId={comment.authorId} pic={comment.author.image}></CommentComponent>
         );
     });
 
@@ -85,13 +85,20 @@ type CommentProps = {
     username: string | null,
     text: string,
     userId: string,
+    pic:  string | null,
 }
 
-function CommentComponent({username, text, userId}: CommentProps){
+function CommentComponent({username, text, userId, pic}: CommentProps){
     return(
-        <div className="w-90 md:w-120 border-b p-1">
-            <a href={"/user/" + userId} className="font-bold hover:text-gray-500">{username}</a>
-            <p className="">{text}</p>
+        <div className="flex flex-col w-90 md:w-120 border-b p-1">
+            <div className="flex items-center">
+                <img src={pic ?? ""} className="w-8 h-8 mr-2"></img>
+                <a href={"/user/" + userId} className="font-bold hover:text-gray-500">{username}</a>
+            </div>
+
+            <div className="grow">
+                <p className="">{text}</p>
+            </div>
         </div>
     );
 }
