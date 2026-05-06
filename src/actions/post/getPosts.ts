17a9c53b6d1a43ed754ff68c9c54acad7caa674d
chritical <christopherkbanks@gmail.com>
2026-05-06 16:19:39 -0400

    Added dynamic post loading on browse page.

--- src/actions/post/getPosts.ts
+++ src/actions/post/getPosts.ts
@@ -1,9 +1,11 @@
 'use server';
 import {prisma} from "@/prisma/prisma";
 
-export async function getPosts(){
+export async function getPosts(cursor: string){
     const postData = await prisma.post.findMany({
         take: 5,
+        skip: cursor ? 1 : 0,   // skip only when cursor exists
+        ...(cursor && { cursor: { id: cursor } }),
         where :{
             NOT: {title: ""}
         },
--- src/components/post/PostBrowser.tsx
+++ src/components/post/PostBrowser.tsx
@@ -1,30 +1,41 @@
 'use client';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import type {Post} from "@/src/actions/post/getPosts"
 import PostBox from "@/src/components/post/PostBox";
 import { getPosts } from "@/src/actions/post/getPosts";
 
 export default function PostBrowser(){
     const [postData, setPostData] = useState<Post[]>([]);
+    const cursor = useRef("");
 
     useEffect(() => {
         loadPosts();
     }, []);
 
     async function loadPosts(){
-        setPostData(await getPosts());
+        const newPosts = await getPosts(cursor.current);
+        setPostData(postData => [...postData, ...newPosts]);
+
+        if(newPosts.length > 0){
+            cursor.current = newPosts[(newPosts.length -1)].id;
+        }
     }
 
-    const postBoxes = postData.map((post) => {
+    const postBoxes = postData.map((post, index) => {
         return(
-            <PostBox key={post.id} post={post} viewOrEdit="view"></PostBox>
+            <PostBox key={post.id + index} post={post} viewOrEdit="view"></PostBox>
         );
     });
+
+    async function handleLoadButton(){
+        loadPosts();
+    }
     
     return(
         <>
             <p>post browser</p>
             {postBoxes}
+            <button onClick={() => handleLoadButton()} className="btn">Load</button>
         </>
     );
 }
