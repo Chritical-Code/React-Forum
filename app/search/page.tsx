6038d790510afacb4eeeeafa684990f93a0eba40
chritical <christopherkbanks@gmail.com>
2026-05-07 14:43:06 -0400

    Fixed search page bug caused by changes to postbox.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,5 @@
 import {prisma} from "@/prisma/prisma";
 import PostBox from "@/src/components/post/PostBox";
-import { Post } from "@/src/generated/prisma/client";
 
 type SearchPageProps = {
     searchParams: Promise<{[key: string]: string | string[] | undefined}>
@@ -13,10 +12,11 @@ export default async function SearchPage({searchParams}: SearchPageProps){
     const posts = await prisma.post.findMany({
         where:{
             OR:[{text: {contains: query, mode: "insensitive"}}, {title: {contains: query, mode: "insensitive"}}]
-        }
+        },
+        include: {postImages: {take: 1}}
     });
     
-    const postBoxes = posts.map((post: Post, index) => {
+    const postBoxes = posts.map((post) => {
         return(
             <PostBox key={post.id} post={post} viewOrEdit="view"></PostBox>
         );
