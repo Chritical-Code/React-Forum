714564a5d79f552d1aceec8f7c3cd15930148ac0
chritical <christopherkbanks@gmail.com>
2026-05-13 17:59:11 -0400

    Removed small gap in postbox.

--- src/components/post/PostBox.tsx
+++ src/components/post/PostBox.tsx
@@ -10,7 +10,7 @@ type PostBoxProps = {
 export default function PostBox({post, viewOrEdit}: PostBoxProps){
     return(
         <a href={"/post/" + viewOrEdit + "/" + post.id} className="inline-flex flex-col items-center m-2 p-0 w-90 md:w-120 h-auto">
-            <div className="flex flex-col items-center shrink-0 bg-gray-400 w-full h-full border m-2 overflow-hidden">
+            <div className="flex flex-col items-center shrink-0 bg-gray-400 w-full h-full border overflow-hidden">
                 <p className="font-bold h-6 w-11/12 overflow-hidden text-center">{post.title}</p>
                 <p className="h-6 w-11/12 overflow-hidden text-center">{post.text}</p>
                 {post.postImages[0] && <MyImg image={post.postImages[0]}></MyImg>}
